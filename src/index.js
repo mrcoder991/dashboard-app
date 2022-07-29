7022de5377fb287d2f2fb0e4120f1eb2a06a9a86
mrcoder991 <udaygirhepunje41@gmail.com>
2022-07-29 20:13:44 +0530

    initial folder structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./index.css"
 import App from './App';
 
-ReactDom.render(
+
+ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
