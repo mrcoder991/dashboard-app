51b02aca2d86715349e0987417836c71f8768950
mrcoder991 <udaygirhepunje41@gmail.com>
2022-07-30 17:02:44 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -72,7 +72,8 @@ const App = () => {
                             <Route path='/area' element={<Area/>} />
                             <Route path='/bar' element={<Bar/>} />
                             <Route path='/pie' element={<Pie/>} />
-                            <Route path='/financial' element={<Financial/>} />
+                            <Route path='/financial' element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
                             <Route path='/pyramid' element={<Pyramid/>} />
                             <Route path='/stacked' element={<Stacked/>} />
 
